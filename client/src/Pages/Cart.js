cfb26a90a1524ff391b6b34e393245f61d7b0c33
Annetaliya <annetaliya@gmail.com>
2023-11-28 22:53:10 +0300

    Added cart items

--- client/src/Pages/Cart.js
+++ client/src/Pages/Cart.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Header from '../Assets/Header.jpg';
+
 
 const Cart = () => {
+  // const productData = useSelector((state) => state.bazar.productData)
+  console.log(product.Data) 
   return (
-    <div>Cart</div>
+    <div>
+      <img src={Header} alt='headerimg' />
+      <div className='max-w-screen-xl mx-auto py-20 flex'>
+        <CartItem />
+        <div className='w-1/3 bg-[#fafafa] py-6 px-4'>
+          <div>
+            <h2 className='text-2xl font-medium'>Cart Totals</h2>
+            <p className='flex items-center gap-4 text-base' >
+              Subtotal{" "}
+              <span className='font-titleFont font-bold text-lg'>
+                $200
+              </span>
+            </p>
+            <p className='flex items-start gap-4 text base'> 
+              Shipping{" "}
+              <span>
+                LOrem, ipsum dolor sit amet consecter adipisicing elit
+              </span>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
