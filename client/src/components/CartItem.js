78968a7277a2a124683b86c209f989c2deb7c01a
Annetaliya <annetaliya@gmail.com>
2023-12-02 19:35:06 +0300

    Added cart items

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdOutlineClose } from 'react-icons/md';
 import { HiOutlineArrowLeft } from 'react-icons/hi';
+import { ToastContainer, toast } from 'react-toastify';
+import { resetCart } from '../redux/bazarSlice';
+import { Link } from 'react-router-dom';
 
 const CartItem = () => {
   const productData = useSelector((state) => state.bazar.productData);
+  const dispatch = useDispatch();
   return (
     <div className='w-2/3 pr-10'> 
         <div className='w-full'>
@@ -16,7 +20,8 @@ const CartItem = () => {
             <div key={item._id}
             className='flex items-center justify-between gap-6 mt-6'>
               <div className='flex items-center gap-2'>
-                <MdOutlineClose
+                <MdOutlineClose onClick={()=> dispatch(deleteItem(item._id)) &
+                toast.error(`${item.title} is removed`)}
                 className='text-xl text-gray-600 hover:text-red-600 cursor-pointer duration-300' />
                 <img 
                   className='w-32 h-32 object-cover'
@@ -26,8 +31,17 @@ const CartItem = () => {
               <p className='w-10'>{item.price}</p>
               <div className='w-52 flex items-center justify-between text-gray-500 gsp-4 border p-3'>
                 <p className='text-sm'>Quantity</p>
-                <div>
+                <div className='flex items-center gap-4 text-sm font-semibold'>
                   <span
+                  onClick={() => 
+                  dispatch(decrementQuantity({
+                    _id: item._id,
+                    title: item.title,
+                    image: item.image,
+                    price: item.price,
+                    quantity: 1,
+                    description: item.description,
+                  }))}
                   className='border h-5 font-normal text-lg flex items-center
                   justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer
                   duration-300 active:bg-black'
@@ -35,6 +49,15 @@ const CartItem = () => {
                     -
                   </span>
                   <span
+                  onClick={() => 
+                  dispatch(decrementQuantity({
+                      _id: item._id,
+                      title: item.title,
+                      image: item.image,
+                      price: item.price,
+                      quantity: 1,
+                      description: item.description,
+                    }))}
                   className='border h-5 font-normal text-lg flex items-center
                   justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer
                   duration-300 active:bg-black'
@@ -47,7 +70,33 @@ const CartItem = () => {
            
             </div>
           ))}
+
         </div>
+        <button onClick={() => dispatch(resetCart())& toast.error('Your Cart is Empty')}
+          className='bg-red-500 tex-white mt-8 ml-7 py-1 px-6 hover:bg-red-800 duration-300'>
+          Reset Cart
+        </button>
+        <Link to='/'>
+            <button className='mt-8 ml-7 flex items-center gap-1 text-gray-400
+            hover:text-black duration-300'>
+              <span>
+                <HiOutlineArrowLeft />
+              </span>
+              go shopping
+            </button>
+        </Link>
+        <ToastContainer
+          position='top-left'
+          autoClose={2000}
+          hideProgressBar={false}
+          newesOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='dark' 
+      />
     </div>
   )
 }
